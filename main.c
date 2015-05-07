dde29fd1645f5a328801ba732c355a9d755a3345
Alex Haslehurst <alex.haslehurst@gmail.com>
2015-05-07 23:00:46 +0100

    PID file

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <bcm2835.h>
 #include <signal.h>
 #include <sys/stat.h>
 #include <sys/syslog.h>
+#include <string.h>
 
 #define PIN_IN RPI_BPLUS_GPIO_J8_38
 #define PIN_OUT RPI_BPLUS_GPIO_J8_40
+#define PID_FILE "/var/run/PiSwitch.pid"
 
 void TermSignalHandler(int signal);
 void StartDaemon();
@@ -59,7 +62,7 @@ int main() {
         delay(2000);
     }
 
-    syslog(LOG_INFO, "stopping", PIN_IN);
+    syslog(LOG_INFO, "stopping");
     closelog();
     bcm2835_close();
     return EXIT_SUCCESS;
@@ -74,48 +77,41 @@ void StartDaemon()
 {
     pid_t pid;
 
-    /* Fork off the parent process */
+    // Fork off the parent process
     pid = fork();
 
-    /* An error occurred */
+    // An error occurred
     if (pid < 0) {
         exit(EXIT_FAILURE);
     }
 
-    /* Success: Let the parent terminate */
+    // Success: Let the parent terminate
     if (pid > 0) {
         exit(EXIT_SUCCESS);
     }
 
-    /* On success: The child process becomes session leader */
+    // On success: The child process becomes session leader
     if (setsid() < 0) {
         exit(EXIT_FAILURE);
     }
 
-    /* Catch, ignore and handle signals */
-    signal(SIGCHLD, SIG_IGN);
-    signal(SIGHUP, SIG_IGN);
-    signal(SIGTERM, TermSignalHandler);
-    signal(SIGKILL, TermSignalHandler);
-    signal(SIGINT, TermSignalHandler);
-
-    /* Fork off for the second time*/
+    // Fork off for the second time
     pid = fork();
 
-    /* An error occurred */
+    // An error occurred
     if (pid < 0) {
         exit(EXIT_FAILURE);
     }
 
-    /* Success: Let the parent terminate */
+    // Success: Let the parent terminate
     if (pid > 0) {
         exit(EXIT_SUCCESS);
     }
 
-    /* Set new file permissions */
+    // Set new file permissions
     umask(0);
 
-    /* Change the working directory to the root directory */
+    // Change the working directory to the root directory
     chdir("/");
 
     /* Close all open file descriptors */
@@ -124,6 +120,30 @@ void StartDaemon()
         close (x);
     }
 
-    /* Open the log file */
+    // Open the log file
     openlog ("PiSwitch", LOG_PID, LOG_DAEMON);
+
+    // Catch, ignore and handle signals
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGTSTP,SIG_IGN);
+    signal(SIGTTOU,SIG_IGN);
+    signal(SIGTTIN,SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+    signal(SIGTERM, TermSignalHandler);
+    signal(SIGINT, TermSignalHandler);
+
+    // Record PID
+    int lfp = open(PID_FILE, O_RDWR | O_CREAT, 0640);
+    if (lfp < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    if (lockf(lfp, F_TLOCK, 0) < 0) {
+        // Can not lock
+        exit(EXIT_SUCCESS);
+    }
+
+    char str[10];
+    sprintf(str, "%d\n", getpid());
+    write(lfp, str, strlen(str));
 }
